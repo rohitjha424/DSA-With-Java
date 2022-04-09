42cd1e063805d23991109e2916dc2f276c2b7178
Rohit Kumar Jha <64392216+rohitjha424@users.noreply.github.com>
2022-04-09 18:19:11 +0530

    Add files via upload

---
+++ String/StringConcat.java
@@ -0,0 +1,31 @@
+// Printing 2D array in the form of matrix
+
+
+
+
+import java.util.*;
+class StringConcat
+{
+	public static void main (String[] args)
+	{	 
+	    String str1 , str2;
+	
+	    Scanner sc = new Scanner(System.in);
+	 
+	 
+	    System.out.println("Enter the 1st String");
+	     
+	    str1 = sc.next();
+	    
+	    System.out.println("Enter the 2nd String:");
+	      
+        str2 = sc.next();
+
+	            
+	           
+	   System.out.print( "The Concatinated String is: " + str1 + " " + str2);
+	        
+	    
+		
+	}
+}
