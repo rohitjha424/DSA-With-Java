23ce5e0a23d9db929818b066caeef0b6b74a95b7
Rohit Kumar Jha <64392216+rohitjha424@users.noreply.github.com>
2022-04-09 20:44:12 +0530

    Add files via upload

---
+++ String/SubStrings.java
@@ -0,0 +1,29 @@
+// SubStrings
+
+
+
+
+import java.util.*;
+class SubStrings
+{
+	public static void main (String[] args)
+	{	 
+	    String str1 , str2;
+	
+	    Scanner sc = new Scanner(System.in);
+	 
+	 
+
+	     
+	    str1 = "12345678901";
+	    int size = str1.length();
+	    
+	    str2 = str1.substring(1,5);
+	    
+	        System.out.println(str2);
+
+	        
+	    
+		
+	}
+}
